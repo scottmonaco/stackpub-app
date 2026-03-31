82b9301a78df6b2f1b63f611bc7fab9d2d47eafc
scottmonaco <bscottmonaco@gmail.com>
2026-03-31 10:31:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -813,6 +813,7 @@ function renderPageShell({ slug, displayName, logoUrl, textStyle, imageFilter, c
 <html lang="en">
 <head>
   <meta charset="UTF-8" />
+  <link rel="icon" href="/favicon.ico" type="image/x-icon" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>${esc(displayName)}</title>
   <meta property="og:title" content="${esc(displayName)}" />
